691c3ebea6b201a62edcfba82ffacb6836d7e747
monoald <carlos.aldazosa@outlook.com>
2024-04-07 21:19:11 -0400

    Singly Linked List: implementation

--- src/Exercises/SinglyLinkedList.java
+++ src/Exercises/SinglyLinkedList.java
@@ -1,4 +1,15 @@
 package Exercises;
 
 public class SinglyLinkedList {
+    private ListNode head;
+
+    private static class ListNode {
+        private int data; // Generic Type
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
