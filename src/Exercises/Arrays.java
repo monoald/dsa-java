a3dcf22b2a066a418e6e6606317eb8e7957c4ac2
monoald <carlos.aldazosa@outlook.com>
2024-04-07 20:41:00 -0400

    Arrays: find missing number

--- src/Exercises/Arrays.java
+++ src/Exercises/Arrays.java
@@ -91,4 +91,14 @@ public final class Arrays {
 
         return resized;
     }
+    public static int findMissingNumber(int[] arr) {
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum -= arr[i];
+        }
+
+        return sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Exercises.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5,9,3,10};
+        int[] arr = {2,5,1,6,3,7};
 
         // int[] oddNumbers = Arrays.removeEven(arr);
         // Arrays.reverse((arr));
@@ -10,7 +10,8 @@ public class Main {
         // int min = Arrays.min(arr);
         // int secondMax = Arrays.secondMax(arr);
         // Arrays.zeroLast(arr);
-        arr = Arrays.resize(arr, 8);
-        Arrays.printInt(arr);
+        // arr = Arrays.resize(arr, 8);
+        int missinNum = Arrays.findMissingNumber(arr);
+        System.out.print(missinNum);
     }
 }
