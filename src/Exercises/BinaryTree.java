e926a83efac9969e655eb2771719beda7923b691
monoald <carlos.aldazosa@outlook.com>
2024-04-14 16:25:17 -0400

    Binary Tree: Iterative preorder traversal

--- src/Exercises/BinaryTree.java
+++ src/Exercises/BinaryTree.java
@@ -1,12 +1,17 @@
 package Exercises;
 
+import com.sun.source.tree.Tree;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class BinaryTree {
     private TreeNode root;
 
     public static void main(String[] args) {
         var binaryTree = new BinaryTree();
         binaryTree.createBinaryTree();
-        binaryTree.preOrder(binaryTree.root);
+        binaryTree.iterativePreOrder();
     }
 
     public BinaryTree () {}
@@ -25,27 +30,54 @@ public class BinaryTree {
     }
 
     public void createBinaryTree() {
-        TreeNode first = new TreeNode(9);
+        TreeNode first = new TreeNode(1);
         TreeNode second = new TreeNode(2);
-        TreeNode third = new TreeNode(4);
-        TreeNode fourth = new TreeNode(3);
-        // TreeNode fifth = new TreeNode(5);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
 
         this.root = first;
         first.left = second;
-        first.right = fourth;
+        first.right = third;
 
-        second.left = third;
-        //second.right = fifth;
+        second.left = fourth;
+        second.right = fifth;
+
+        third.left = sixth;
+        third.right = seventh;
     }
 
-    public void preOrder(TreeNode root) {
+    public void recursivePreOrder(TreeNode root) {
         if (root == null) {
             return;
         }
 
         System.out.print(root.data + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+        recursivePreOrder(root.left);
+        recursivePreOrder(root.right);
+    }
+
+    public void iterativePreOrder() {
+        if (root == null) {
+            return;
+        }
+
+        var stack = new Stack<TreeNode>();
+        stack.push(this.root);
+
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            System.out.print(temp.data + " - ");
+
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
     }
 }
