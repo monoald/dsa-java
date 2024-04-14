b46b92c51106e8bcee24dd3f8419e53c7da56d17
monoald <carlos.aldazosa@outlook.com>
2024-04-14 09:16:44 -0400

    Stack: reverse string with stack

--- src/Exercises/ReverseStringWithStack.java
+++ src/Exercises/ReverseStringWithStack.java
@@ -1,4 +1,25 @@
 package Exercises;
 
+import java.util.Stack;
+
 public class ReverseStringWithStack {
+    public static void main(String[] args) {
+        var name = "Berlin";
+        System.out.println(name);
+        System.out.println(reverseString(name));
+    }
+
+    public static String reverseString(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] charArr = str.toCharArray();
+
+        for (int i = 0; i < charArr.length; i++) {
+            stack.push(charArr[i]);
+        }
+        for (int i = 0; i < charArr.length; i++) {
+            charArr[i] = stack.pop();
+        }
+
+        return new String(charArr);
+    }
 }
